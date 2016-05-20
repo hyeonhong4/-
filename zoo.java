8f331ea073bd8f0706a4e7a63777abb40b7ccdf2
hyeonhong4 <gusghd14@naver.com>
2016-05-20 13:13:27 +0900

    Update zoo.java

--- zoo.java
+++ zoo.java
@@ -1,22 +1,34 @@
-import java.lang.*;
+import java.util.*;
 public class zoo{
 
   public static void main(String[] args){
-    Animal[] zoo = new Animal[5];
-    zoo[0]=new Animal("»çÀÚ","¾îÈï");
-    zoo[1]=new Animal("°í¾çÀÌ","¾ß¿Ë");
-    zoo[2]=new Animal("Áã","¨t¨t");
-    zoo[3]=new Animal("°³","¸Û¸Û");
-    zoo[4]=new Animal("µÅÁö","²Ü²Ü");
+    
+    Scanner my_zoo_number = new Scanner(System.in);
+    System.out.println("What Animal Number?");
+    int number = my_zoo_number.nextInt();
+    
+    Animal[] zoo = new Animal[number];
+    
+    for(int i=0;i<zoo.length;i++){
+    
+    Scanner my_zoo_animal = new Scanner(System.in);
+    System.out.println((i+1)+" - What Animal?");
+    String my_animal = my_zoo_animal.nextLine();
+    
+    Scanner my_zoo_sound = new Scanner(System.in);
+    System.out.println(my_animal+" What Sound?");
+    String my_sound = my_zoo_sound.nextLine();
+    
+    zoo[i]=new Animal(my_animal,my_sound);
+    
+    }
     
     all_sounds(zoo);
-    
-    
   }
   
   
   public static void all_sounds(Animal[] zoo){
-    for(int i=0;i<=4;i++){
+    for(int i=0;i<zoo.length;i++){
     System.out.println("The "+zoo[i].name+" gose "+zoo[i].sound);
                          }
   }
